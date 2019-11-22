3df992007f38d56260f1d80e2010ebf4dc27733f
Jay Kim <yeonjae.jay.kim@gmail.com>
2019-11-22 00:01:28 -0800

    Module 08 completed

--- module08/files/provisioning.sh
+++
@@ -1,6 +0,0 @@
-# Configure sudoers file
-sed -i 's/^#\s*\(%wheel\s*ALL=(ALL)\s*NOPASSWD:\s*ALL\)/\1/' /etc/sudoers
-sed -i 's/^\(Defaults    requiretty\)/#\1/' /etc/sudoers
-
-# Application Setup
-echo "module.exports = {localUrl: 'mongodb://localhost/acit4640'};" > /home/todo-app/app/config/database.js
